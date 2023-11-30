0e1aa650a7fc61cd5ae5c1f7d57ac3b281e8a408
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-11-29 21:52:38 -0300

    add: calculate months old too

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-function calculateYear() {
+function calculateYearMonth() {
   const birth = document.querySelector('#birth').value;
+  const date = new Date();
 
-  const actualYear = new Date().getFullYear();
+  let actualYear = date.getFullYear();
   const birthYear = Number(birth.substr(0, 4));
 
+  let actualMonth = date.getMonth() + 1;
+  const birthMonth = Number(birth.substr(5, 2));
+
   const output = document.querySelector('#output');
-  const yearsOld = actualYear - birthYear;
-  output.innerText = `You have ${yearsOld} years old`;
+  let monthsOld = actualMonth - birthMonth;
+  // 8 - 10 = -2 two months left to complete
+  if (actualMonth < birthMonth) {
+    actualYear--;
+    actualMonth = 12 + actualMonth;
+    monthsOld = actualMonth - birthMonth
+  }
+
+  let yearsOld = actualYear - birthYear;
+
+  // 11 - 10 = 1
+  console.log(monthsOld)
+
+  output.innerText = `You have ${yearsOld} years old\nYou have ${monthsOld} months old`;
 }
 
 const btCalculate = document.querySelector('#btCalculate');
-btCalculate.addEventListener('click', calculateYear);
+btCalculate.addEventListener('click', calculateYearMonth);
